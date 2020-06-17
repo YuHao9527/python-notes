76f6283ce083b54e5630dbcaf87ea77043f1a018
YuHao9527 <1127620991@qq.com>
2020-06-17 15:22:45 +0800

    wsgi add

---
+++ Web开发/wsgi_test1.py
@@ -0,0 +1,24 @@
+from wsgiref.simple_server import make_server
+
+# A relatively simple WSGI application. It's going to print out the
+# environment dictionary after being updated by setup_testing_defaults
+def simple_app(environ, start_response):
+    print(environ)
+
+    status = '200 OK'
+    headers = [('Content-type', 'text/plain; charset=utf-8')]
+
+    start_response(status, headers)
+
+    ret = [("%s: %s\n" % (key, value)).encode("utf-8")
+           for key, value in environ.items()]
+    return ret # 返回要求可迭代对象，正文就是这个列表的元素，可以是一个元素一个字符串
+
+with make_server('0.0.0.0', 9000, simple_app) as httpd: # 创建server
+    try:
+        httpd.serve_forever()
+    except Exception as e:
+        print(e)
+    except KeyboardInterrupt:
+        print('stop')
+        httpd.serve_close()
