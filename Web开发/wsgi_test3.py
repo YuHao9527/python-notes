c7c0c24386b4793cb3a7fb3a9496eef8001d7d18
YuHao9527 <1127620991@qq.com>
2020-06-19 15:34:50 +0800

    fix

--- Web开发/wsgi_test3.py
+++ Web开发/wsgi_test3.py
@@ -1,5 +1,6 @@
 from wsgiref.simple_server import make_server
 from webob import Request, Response
+from webob.dec import wsgify
 
 # A 127.0.0.1:9000?id=1&name=tome&age=20
 # 127.0.0.1:9000?id=1&name=tome,jerry&age=20&age=30
@@ -17,15 +18,26 @@ def simple_app(environ:dict, start_response):
     print(request.params) # 所有参数
     print(request.headers) # 请求头
     
-    status = '200 OK'
-    headers = [('Content-type', 'text/plain; charset=utf-8')]
+    res = Response()
+    print(res.status_code) # 200
+    print(res.status) # 200 ok
+    print(res.headers) # object
+    print(res.headerlist) # list
 
-    start_response(status, headers)
+    # status = '200 OK'
+    # headers = [('Content-type', 'text/plain; charset=utf-8')]
 
-    ret = []
-    return ret # 返回要求可迭代对象，正文就是这个列表的元素，可以是一个元素一个字符串
+    # start_response(res.status, res.headerlist)
 
-with make_server('0.0.0.0', 9000, simple_app) as httpd: # 创建server
+    res.body = '<h1>你好</h1>'.encode()
+    # return ret # 返回要求可迭代对象，正文就是这个列表的元素，可以是一个元素一个字符串
+    return res(environ, start_response) # __call__ => iter
+
+@wsgify
+def app(request:Request) -> Response: # 一个请求对应一个响应
+    return Response('<h1>fateGod</h1>')
+
+with make_server('0.0.0.0', 9000, app) as httpd: # 创建server
     try:
         httpd.serve_forever()
     except Exception as e:
